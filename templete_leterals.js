200dc86c333efe024951d0d751c8d5f17f24109a
sneha7040 <snehamondal7040@gmail.com>
2022-02-08 13:22:32 +0530

    updatatempleteleterals

--- templete_leterals.js
+++ templete_leterals.js
@@ -1,7 +1,22 @@
- //es5
- console.log("My Name is Sneha.  My age is 22.");
- //es6
- console.log(`My Name is Sneha. My age is 22.`);
-  
-  
+//es5
+console.log("My Name is Sneha.  My age is 22.");
+//es6
+console.log(`My Name is Sneha. My age is 22.`);
 
+//  es5
+let firstName = "Sneha";
+let lastName = "Mondal";
+// console.log("my name is "+firstName+" "+lastName);
+
+// es6
+console.log(`my name is :${firstName} ${lastName}`);
+
+let a = 20;
+let b = 30;
+let c = a + b;
+console.log(a + b + " and\n not " + (2 * a + b));
+
+console.log(
+  `${a + b} and
+not ${2 * a + b}`
+);
