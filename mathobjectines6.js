b9a049b4cdbecf6551ab25e554645717319c89f4
sneha7040 <snehamondal7040@gmail.com>
2022-02-21 11:47:40 +0530

    math object in es6

--- mathobjectines6.js
+++ mathobjectines6.js
@@ -1,5 +1,26 @@
-let number =9.8587;
-console.log(Math.sign(number));
-console.log(Math.trunc(number));
 
-console.log(Math.floor(number));
+
+// math function sign(), trunc(), floor()
+ let number =9.8587;
+// console.log(Math.sign(number));
+// console.log(Math.trunc(number));
+// console.log(Math.floor(number));
+///................................................................
+// exponential operator
+
+/*x ** y  thi si is produce the same result of pow(x,y)*/
+
+let a=5;
+let b=3;
+console.log(a**b);
+
+//number and global method
+// isFinite()
+// isNaN()
+// isInteger()
+
+let num=5;
+console.log(Number.isFinite(num));
+console.log(Number.isNaN());
+console.log(Number.isInteger(num));
+
