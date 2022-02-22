ba29569b7b5bcda9e64d7e28bfe9174e55e58e22
sneha7040 <snehamondal7040@gmail.com>
2022-02-22 12:30:54 +0530

    synchronous and asynchronous

---
+++ sync&async.js
@@ -0,0 +1,42 @@
+const pob = new Promise( (resolve,reject)=>{
+ setTimeout(()=>{
+let roll_No=[1,2,3,4,5];
+resolve(roll_No);
+ },2000);
+});
+
+const getBiodata=(indexData)=>{
+    return new Promise((resolve,reject)=>{
+         setTimeout((indexData)=>{
+            let Biodata={
+                name:"sneha mondal",
+                age:"22"
+
+            }
+            resolve(`my name is ${Biodata.name} and I am ${Biodata.age} years old.`);
+         },2000, indexData);
+    });
+}
+
+// pob.then((rollNo)=>{
+//   console.log(rollNo);
+//   return getBiodata(rollNo[1]);
+// }).then((anything)=>{
+// console.log(anything);
+// }).catch((error)=>{
+//   console.log(error);
+// })
+ async function getData(){
+      const rollNoData =await pob;
+      console.log(rollNoData);
+      
+      const bioDatas= await getBiodata(rollNoData[1]);
+      console.log(bioDatas);
+
+       return bioDatas;
+      
+ }
+ const getName = getData().then((myname)=>{
+    console.log(myname);
+ });
+ 
