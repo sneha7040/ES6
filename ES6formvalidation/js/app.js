0324590d54fe95d804eb6e95e53a6b6739e07dd5
sneha7040 <snehamondal7040@gmail.com>
2022-03-01 11:28:21 +0530

    validation

---
+++ ES6formvalidation/js/app.js
@@ -0,0 +1,5 @@
+const username = document.querySelector("#username");
+const username = document.querySelector("#email");
+const username = document.querySelector("#password");
+const username = document.querySelector("#confirm-password");
+const signup = document.querySelector('#signup');
